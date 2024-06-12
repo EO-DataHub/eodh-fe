50c1fc0517846d9fb7dfd8f1978c9fa42c229234
sfx <sfx>
2024-06-12 09:48:05 +0200

    innitial commit

---
+++ apps/eodh-fe/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="eodh-fe" />
+    </StyledApp>
+  );
+}
+
+export default App;
