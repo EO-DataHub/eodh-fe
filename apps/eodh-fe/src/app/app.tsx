de3927d752c564a8e659e3201dc81d98025e5982
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-07-04 16:06:50 +0200

    chore: added and configured react query with TS types and eslint for it

--- apps/eodh-fe/src/app/app.tsx
+++ apps/eodh-fe/src/app/app.tsx
@@ -1,10 +1,18 @@
 import { DisplayMap } from './map.component';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient()
 
 export function App() {
   return (
-    <div className="bg-gray-100">
-      <DisplayMap />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="bg-gray-100">
+        <DisplayMap />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    
   );
 }
 
