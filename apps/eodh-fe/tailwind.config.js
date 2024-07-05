614a4a22e3f0a4bb300f81da778d36e002c83f5f
mludwig <mlu@spyro-soft.com>
2024-07-05 10:57:31 +0200

    feat: move tailwind configuration into eodh app

--- apps/eodh-fe/tailwind.config.js
+++ apps/eodh-fe/tailwind.config.js
@@ -4,8 +4,6 @@ const { join } = require('path');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './apps/**/*.{js,jsx,ts,tsx}',
-    './libs/**/*.{js,jsx,ts,tsx}',
     join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
