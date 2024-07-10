f19a76740b9a4decc21504310e2ae22e9743f920
mludwig <mlu@spyro-soft.com>
2024-07-10 13:29:26 +0200

    feat: move map component into library

---
+++ apps/storybook-host/tailwind.config.js
@@ -0,0 +1,15 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'libs/**/{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,js,jsx,html}',
+    join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- libs/map/ui-map/src/lib/map.component.stories.tsx
+++ libs/map/ui-map/src/lib/map.component.stories.tsx
@@ -2,20 +2,23 @@ import { expect } from '@storybook/jest';
 import type { Meta, StoryObj } from '@storybook/react';
 import { within } from '@storybook/testing-library';
 
-import { DisplayMap } from './map.component';
+import { Map, MapWrapper } from './map.component';
 
-const meta: Meta<typeof DisplayMap> = {
-  component: DisplayMap,
-  title: 'apps/eodh-fe/Map',
+const meta: Meta<typeof Map> = {
+  component: Map,
+  title: 'libs/map/ui-map/Map',
+  decorators: [
+    (Story) => (
+      <MapWrapper>
+        <Story />
+      </MapWrapper>
+    ),
+  ],
 };
 export default meta;
-type TStory = StoryObj<typeof DisplayMap>;
+type TStory = StoryObj<typeof Map>;
 
-export const Primary = {
-  args: {},
-};
-
-export const Heading: TStory = {
+export const MapComponent: TStory = {
   args: {},
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
