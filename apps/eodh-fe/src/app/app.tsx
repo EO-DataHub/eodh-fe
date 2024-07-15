004b0f2946a5eeecbf303138f9a0f9fe15502656
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-07-15 09:06:13 +0200

    chore: restored App

--- apps/eodh-fe/src/app/app.tsx
+++ apps/eodh-fe/src/app/app.tsx
@@ -3,15 +3,11 @@ import { withQueryClient } from '@ukri/shared/utils/react-query';
 
 export function App() {
   return (
-    <div className='bg-bright-dark text-base-content p-4 text-small-semibold'>
-      <h1 className='text-4xl'>Welcome to My App!</h1>
-      <p className='mt-2'>This is a sample application using custom TailwindCSS theme.</p>
+    <div className='bg-gray-100' data-testid='app-root'>
+      <MapWrapper>
+        <Map />
+      </MapWrapper>
     </div>
-    // <div className='bg-gray-100' data-testid='app-root'>
-    //   <MapWrapper>
-    //     <Map />
-    //   </MapWrapper>
-    // </div>
   );
 }
 
