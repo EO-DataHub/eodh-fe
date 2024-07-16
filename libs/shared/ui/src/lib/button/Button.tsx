411c46aac127d66207aaecad5f9290d4c2e0cdd9
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-07-16 14:20:03 +0200

    build: added ui-components library with button

---
+++ libs/shared/ui/src/lib/button/Button.tsx
@@ -0,0 +1,11 @@
+import styles from './Button.module.css';
+
+export function Button() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Button!</h1>
+    </div>
+  );
+}
+
+export default Button;
