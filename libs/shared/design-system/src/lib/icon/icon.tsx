125d0972ced6f5ea4e4abf76638db2507c6a5db0
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-07-31 11:14:29 +0200

    fix: linting

--- libs/shared/design-system/src/lib/icon/icon.tsx
+++ libs/shared/design-system/src/lib/icon/icon.tsx
@@ -10,11 +10,6 @@ interface IIconProps {
 export const Icon = ({ name = 'Bolt', width, height, className }: IIconProps) => {
   const IconComponent = IconsNames[name];
 
-  if (!IconComponent) {
-    console.warn(`Icon "${name}" not found`);
-    return null;
-  }
-
   return (
     <div className={`min-w-6 flex justify-center items-center ${className}`}>
       <IconComponent width={width as number} height={height as number} />
