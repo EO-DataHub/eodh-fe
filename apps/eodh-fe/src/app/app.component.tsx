8a682281c0be47d0296876f413ffd4c3c4eb2530
mludwig <mlu@spyro-soft.com>
2024-08-12 12:44:58 +0200

    chore: rename search component

--- apps/eodh-fe/src/app/app.component.tsx
+++ apps/eodh-fe/src/app/app.component.tsx
@@ -4,7 +4,7 @@ import { Suspense } from 'react';
 import { I18nextProvider } from 'react-i18next';
 
 import i18n from '../i18n';
-import { SearchLayout } from './layout/search.component';
+import { SearchLayout } from './layout/search-layout.component';
 
 export function App() {
   return (
