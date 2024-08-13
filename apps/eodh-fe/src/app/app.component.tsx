20b95c05eae7cc089a0edb0717483d14fae6e491
mludwig <mlu@spyro-soft.com>
2024-08-13 09:41:28 +0200

    feat: add test api call

--- apps/eodh-fe/src/app/app.component.tsx
+++ apps/eodh-fe/src/app/app.component.tsx
@@ -1,12 +1,21 @@
 import { AppLoader } from '@ukri/shared/design-system';
 import { withQueryClient } from '@ukri/shared/utils/react-query';
-import { Suspense } from 'react';
+import Axios from 'axios';
+import { Suspense, useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
 
 import i18n from '../i18n';
 import { SearchLayout } from './layout/search-layout.component';
 
 export function App() {
+  useEffect(() => {
+    Axios.create()
+      .get('https://test.eodatahub.org.uk/api/catalogue/stac/collections?limit=99999&q=')
+      .then((response) => {
+        console.log('data', response);
+      });
+  }, []);
+
   return (
     <Suspense fallback={<AppLoader />}>
       <I18nextProvider i18n={i18n}>
