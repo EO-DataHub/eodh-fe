de89c82e381ef4575dc4709d52f4f62a4d13dfd3
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-08-23 12:04:00 +0200

    style: icon svg - center

--- libs/shared/design-system/src/lib/input/date-input/date-input.styles.tsx
+++ libs/shared/design-system/src/lib/input/date-input/date-input.styles.tsx
@@ -5,5 +5,5 @@ export const dateInputStyles = {
     `flex-grow px-2 py-[7px] text-text text-medium-regular focus:outline-none border rounded focus:outline-none focus:ring-1 focus:ring-primary-light focus:border-primary-light ${
       error ? 'border-error' : 'border-bright-dark'
     } calendar-picker-indicator:bg-red`,
-  icon: 'text-neutral-light items-end absolute z-10 relative right-[25px] bottom-[-4px] pointer-events-none bg-bright-main',
+  icon: 'text-neutral-light items-end absolute z-10 relative right-[30px] pointer-events-none bg-bright-main',
 };
