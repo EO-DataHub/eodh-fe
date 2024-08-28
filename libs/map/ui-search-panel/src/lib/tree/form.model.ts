6839277376cb64c84e53325bb6869195414965af
mludwig <mlu@spyro-soft.com>
2024-08-28 09:21:12 +0200

    feat: wip

--- libs/map/ui-search-panel/src/lib/tree/form.model.ts
+++ libs/map/ui-search-panel/src/lib/tree/form.model.ts
@@ -2,17 +2,37 @@ export type TForm = {
   copernicus: {
     sentinel1: {
       acquisitionMode: {
-        EW: boolean;
-        HH: boolean;
-        'HH+HV': boolean;
-        IW: boolean;
-        VV: boolean;
-        'VV+VH': boolean;
+        ew: boolean;
+        hh: boolean;
+        hh_hv: boolean;
+        iw: boolean;
+        vv: boolean;
+        vv_vh: boolean;
       };
       orbitDirection: {
         ascending: boolean;
         descending: boolean;
       };
+    };
+    sentinel2: {
+      l1c: boolean;
+      l2a: boolean;
+      cloudCoverage: number;
+    };
+    sentinel3: {
+      slstr: boolean;
+      cloudCoverage: number;
+      olci: boolean;
+    };
+    sentinel5: {
+      aer_ai: boolean;
+      ch4: number;
+      cloud: boolean;
+      co: boolean;
+      hcho: boolean;
+      no2: boolean;
+      o3: boolean;
+      so2: boolean;
     };
   };
 };
--- libs/map/ui-search-panel/src/lib/tree/tree.component.tsx
+++ libs/map/ui-search-panel/src/lib/tree/tree.component.tsx
@@ -1,13 +1,24 @@
 import { Tree as TreeWrapper } from '@ukri/shared/design-system';
+import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
 
+import { TForm } from './form.model';
 import { PrivateData } from './private-data.component';
 import { PublicData } from './public-data.component';
 
-export const Tree = () => {
+type TTreeProps = {
+  onSubmit: (data: TForm) => unknown | Promise<unknown>;
+};
+
+export const Tree = ({ onSubmit }: TTreeProps) => {
+  const { handleSubmit } = useForm<TForm>();
+
   return (
     <TreeWrapper>
-      <PublicData />
-      <PrivateData />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <PublicData />
+        <PrivateData />
+      </form>
     </TreeWrapper>
   );
 };
