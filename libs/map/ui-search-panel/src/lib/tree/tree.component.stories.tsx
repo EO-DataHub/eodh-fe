c4afea4859ac836c7e6c21ae5494c3d331d1f7db
mludwig <mlu@spyro-soft.com>
2024-08-30 14:21:25 +0200

    fix: fix storybook

--- libs/map/ui-search-panel/src/lib/tree/tree.component.stories.tsx
+++ libs/map/ui-search-panel/src/lib/tree/tree.component.stories.tsx
@@ -1,9 +1,31 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import type { Meta } from '@storybook/react';
+import { useCallback } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 
+import { defaultValues } from './form.default-data';
+import { TForm } from './form.model';
+import { validationSchema } from './form.schema';
 import { Tree as TreeComponent } from './tree.component';
 
 const TreeTemplate = () => {
-  return <TreeComponent />;
+  const form = useForm<TForm>({
+    defaultValues,
+    resolver: zodResolver(validationSchema),
+    reValidateMode: 'onChange',
+    mode: 'onChange',
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const submit = useCallback(() => {}, []);
+
+  return (
+    <FormProvider {...form}>
+      <form onSubmit={form.handleSubmit(submit)}>
+        <TreeComponent />
+      </form>
+    </FormProvider>
+  );
 };
 
 const meta: Meta<typeof TreeTemplate> = {
