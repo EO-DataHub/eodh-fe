57070c2617c8791940279ac4eb212ed374745832
mludwig <mlu@spyro-soft.com>
2024-09-03 17:53:52 +0200

    fix: fix storybook tests

--- apps/storybook-host/.storybook/preview.tsx
+++ apps/storybook-host/.storybook/preview.tsx
@@ -1,7 +1,8 @@
 import './main.css';
 
+import { QueryClientProvider } from '@tanstack/react-query';
 import { AuthInterceptor, AuthProvider, KeycloakAdapter } from '@ukri/shared/utils/authorization';
-import { initHttpClient } from '@ukri/shared/utils/react-query';
+import { initHttpClient, queryClient } from '@ukri/shared/utils/react-query';
 import { ComponentType, Suspense, useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
 
@@ -23,13 +24,15 @@ const WithI18next = (Story: ComponentType, context: { globals: IStorybookGlobalC
   }, [locale]);
 
   return (
-    <AuthProvider adapter={keycloakAdapter}>
-      <Suspense fallback={<div>loading translations...</div>}>
-        <I18nextProvider i18n={i18n}>
-          <Story />
-        </I18nextProvider>
-      </Suspense>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider adapter={keycloakAdapter}>
+        <Suspense fallback={<div>loading translations...</div>}>
+          <I18nextProvider i18n={i18n}>
+            <Story />
+          </I18nextProvider>
+        </Suspense>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
