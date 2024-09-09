62e9a6597f11bdde535d636afa1fd671a8946cc1
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-09-09 16:01:46 +0200

    chore: updated naming, fixed build

--- libs/map/ui-map/src/lib/stac-layer/stac-layer.component.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-import { useStacLayer } from './use-stac-layer.hook';
-
-export const StacLayer = ({ children }: PropsWithChildren) => {
-  useStacLayer();
-
-  return children;
-};
