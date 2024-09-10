17c6d589668bfbb4591d98d7f4e81f94752d9363
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-09-10 09:01:43 +0200

    style: updated min-width and box sizing

--- apps/eodh-fe/src/app/layout/top-bar.component.tsx
+++ apps/eodh-fe/src/app/layout/top-bar.component.tsx
@@ -6,7 +6,7 @@ import { Logo } from './logo.component';
 export const TopBar = () => {
   return (
     <div className='w-full bg-background border-b-[1px] border-bright-mid flex items-center text-text divide-bright-mid divide-x divide-x-reverse'>
-      <div className='w-[360px] h-full bg-bright-main flex items-center border-bright-mid border-r-[1px]'>
+      <div className='w-[360px] box-content h-full bg-bright-main flex items-center border-bright-mid border-r-[1px]'>
         <a className='ml-4 my-4 no-underline' href='/'>
           <Logo className='h-11' />
         </a>
