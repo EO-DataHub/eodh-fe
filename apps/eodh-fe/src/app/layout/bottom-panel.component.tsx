961eddd853ba9befeb5740becf55efdb2b9842b4
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-11-06 14:49:23 +0100

    fix: updated date check

--- apps/eodh-fe/src/app/layout/bottom-panel.component.tsx
+++ apps/eodh-fe/src/app/layout/bottom-panel.component.tsx
@@ -15,7 +15,7 @@ export const BottomPanel = ({ className }: IBottomPanel) => {
         className={`w-full h-[76px] bg-background-main border-b-[1px] border-bright-dark flex items-center text-text bottom-0 ${className}`}
       >
         <TimeSlider
-          min={createDateString('2000-01-01T00:00:00+00:00')}
+          min={createDateString('2000-01-01')}
           max={createDateString('2001-01-01T00:00:00+00:00')}
           className='grow'
         />
--- libs/shared/utils/date/src/lib/date.utils.ts
+++ libs/shared/utils/date/src/lib/date.utils.ts
@@ -127,12 +127,19 @@ const checkValidDateStr = (str: string): str is TDateStringInternal => {
   return false;
 };
 
-const checkValidDateTimeStr = (str: string): str is TDateStringInternal => {
-  if (str.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z$/) !== null) {
-    return true;
+const checkValidDateTimeStr = (dateTimeStr: string): dateTimeStr is TDateStringInternal => {
+  const iso8601Regex = /^(\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2}))?)$/;
+
+  if (!iso8601Regex.test(dateTimeStr)) {
+    return false;
   }
 
-  return str.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/) !== null;
+  const date = new Date(dateTimeStr);
+  if (isNaN(date.getTime())) {
+    return false;
+  }
+
+  return true;
 };
 
 export function createDateString(date?: Date | string): TDateTimeString {
