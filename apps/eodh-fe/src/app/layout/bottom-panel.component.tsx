7bc2024c0241459c2612af2bf7aec64554273644
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-11-06 13:03:57 +0100

    fix: small updates

--- apps/eodh-fe/src/app/layout/bottom-panel.component.tsx
+++ apps/eodh-fe/src/app/layout/bottom-panel.component.tsx
@@ -1,6 +1,6 @@
 import { useMode } from '@ukri/map/data-access-map';
 import { TimeSlider } from '@ukri/shared/ui/time-slider';
-import { type TDateString } from '@ukri/shared/utils/date';
+import { createDateString } from '@ukri/shared/utils/date';
 
 interface IBottomPanel {
   className?: string;
@@ -9,15 +9,18 @@ interface IBottomPanel {
 export const BottomPanel = ({ className }: IBottomPanel) => {
   const { view } = useMode();
 
-  if (view !== 'results') {
-    return null;
+  if (view === 'results') {
+    return (
+      <div
+        className={`w-full h-[76px] bg-background-main border-b-[1px] border-bright-dark flex items-center text-text bottom-0 ${className}`}
+      >
+        <TimeSlider
+          min={createDateString('2000-01-01T00:00:00+00:00')}
+          max={createDateString('2001-01-01T00:00:00+00:00')}
+          className='grow'
+        />
+      </div>
+    );
   }
-
-  return (
-    <div
-      className={`w-full h-[76px] bg-background-main border-b-[1px] border-bright-dark flex items-center text-text bottom-0 ${className}`}
-    >
-      <TimeSlider min={'2000-01-01' as TDateString} max={'2001-01-01' as TDateString} className='grow' />
-    </div>
-  );
+  return null;
 };
