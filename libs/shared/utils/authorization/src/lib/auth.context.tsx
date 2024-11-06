eb84a6277bd6ff66ce1339eb6ef29c4c69656c63
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-11-06 09:07:12 +0100

    fix: code review changes

--- libs/shared/utils/authorization/src/lib/auth.context.tsx
+++ libs/shared/utils/authorization/src/lib/auth.context.tsx
@@ -6,12 +6,10 @@ export type TAuthContextProps<I extends IBaseIdentityClaims, T extends IAuthAdap
   authClient?: T;
   initialized: boolean;
   authenticated: boolean;
-  userWorkspace?: string;
 };
 
 export const AuthContext = createContext({
   initialized: false,
   authenticated: false,
   authClient: undefined,
-  userWorkspace: undefined,
 });
