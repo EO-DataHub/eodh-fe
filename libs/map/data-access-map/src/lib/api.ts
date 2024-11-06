016000432020f7ed47afd508ae9ac685f859dd43
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-11-06 13:53:26 +0100

    chore: updates

--- libs/map/data-access-map/src/lib/api.ts
+++ libs/map/data-access-map/src/lib/api.ts
@@ -1,5 +1,7 @@
 const functions = '/action-creator/functions';
 const history = '/action-creator/submissions';
+const collectionInfo =
+  '/catalogs/user-datasets/{user_workspace}/processing-results/{catalog_id}/collections/{collection_id}';
 
 const eodhProApiUrl = 'EODH_PRO_API_URL';
 
@@ -7,10 +9,11 @@ type TApi = 'EODH_PRO_API_URL' | 'EODH_STAC_API_URL';
 
 type TPath = `${TApi}/${string}`;
 
-type TQueryKey = Record<'PRESETS' | 'FUNCTIONS' | 'WORKFLOW', TPath>;
+type TQueryKey = Record<'PRESETS' | 'FUNCTIONS' | 'WORKFLOW' | 'COLLECTION_INFO', TPath>;
 
 export const paths: TQueryKey = {
   PRESETS: `${eodhProApiUrl}${functions}`,
   FUNCTIONS: `${eodhProApiUrl}${functions}`,
   WORKFLOW: `${eodhProApiUrl}${history}`,
+  COLLECTION_INFO: `${eodhProApiUrl}${collectionInfo}`,
 };
