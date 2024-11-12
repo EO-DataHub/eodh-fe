d68264d815e29e049aa69395d8e8e6a5bef3ef5c
mludwig <mlu@spyro-soft.com>
2024-11-12 14:13:21 +0100

    fix: fix not resetting view after switching tabs

--- libs/map/feature-action-creator-panel/src/lib/content/presets/presets.component.tsx
+++ libs/map/feature-action-creator-panel/src/lib/content/presets/presets.component.tsx
@@ -1,4 +1,4 @@
-import { TPreset, useActionCreator, useGetPresets } from '@ukri/map/data-access-map';
+import { TPreset, useActionCreator, useGetPresets, useMode } from '@ukri/map/data-access-map';
 import { Error, LoadingSpinner } from '@ukri/shared/design-system';
 import { PropsWithChildren, useCallback, useContext } from 'react';
 
@@ -34,14 +34,16 @@ export const Presets = () => {
   const { setActiveTab } = useContext(ActionCreator);
   const { data, error, isLoading, refetch } = useGetPresets();
   const { loadPreset } = useActionCreator();
+  const { changeView } = useMode();
 
   const handleLoadPreset = useCallback(
     (preset: TPreset) => {
       loadPreset({ dataSet: preset.defaultValues.dataSet, functions: preset.defaultValues.functions });
+      changeView('search');
       setActiveTab('workflow');
       return;
     },
-    [loadPreset, setActiveTab]
+    [changeView, loadPreset, setActiveTab]
   );
 
   if (isLoading) {
