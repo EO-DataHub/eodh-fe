60aa21b9d7764ce64410b641b22a3834ed597e7b
mludwig <mlu@spyro-soft.com>
2024-11-18 18:59:47 +0100

    chore: remove unused action creator schema file

--- libs/map/data-access-map/src/lib/form-builder/tree/schema/action-creator.schema.ts
+++
@@ -1,5 +0,0 @@
-import { TDynamicTreeModel } from '../tree-dynamic.model';
-import { privateSchema } from './private.schema';
-import { publicActionCreatorSchema } from './public.schema';
-
-export const treeModel: TDynamicTreeModel = [publicActionCreatorSchema, privateSchema];
