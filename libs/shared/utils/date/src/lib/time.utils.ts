75c0b2a3894d6c72fc2a20fad0f30cfb68eb797c
Katarzyna Paszko <pkatarzynapaszko@gmail.com>
2024-11-20 12:04:39 +0100

    UKRIW-402 - fixed time format bug (#108)
    * fix: fixed styling, fixed time format
    
    * fix: update styling

--- libs/shared/utils/date/src/lib/time.utils.ts
+++ libs/shared/utils/date/src/lib/time.utils.ts
@@ -1,10 +1,13 @@
 import { createDate } from './date.utils';
 import { TDateStringInternal, TDateTimeString } from './date.utils';
 
-export type THourFormat = 'HH:mm';
+export type THourFormat = 'HH:mm' | 'HH:mm:ss';
 
 const checkValidHourStr = (str: string): str is TDateStringInternal => {
   if (str.match(/^\d{2}:\d{2}$/) !== null) {
+    return true;
+  }
+  if (str.match(/^\d{2}:\d{2}:\d{2}$/) !== null) {
     return true;
   }
 
@@ -25,6 +28,46 @@ export const formatHour = (date: TDateTimeString, format: THourFormat = 'HH:mm')
       }
       hourString = `${dateFormatted.getHours().toString().padStart(2, '0')}:${dateFormatted
         .getMinutes()
+        .toString()
+        .padStart(2, '0')}`;
+      break;
+    }
+  }
+
+  if (checkValidHourStr(hourString)) {
+    return hourString;
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(`[DATE UTILS] Invalid date string: ${date}`);
+  return null;
+};
+
+export const formatHourInUtc = (date: TDateTimeString, format: THourFormat = 'HH:mm:ss') => {
+  if (!date) {
+    return null;
+  }
+  let hourString = '';
+
+  switch (format) {
+    case 'HH:mm:ss': {
+      const dateFormatted = createDate(date);
+      if (!dateFormatted) {
+        return null;
+      }
+      hourString = `${dateFormatted.getUTCHours().toString().padStart(2, '0')}:${dateFormatted
+        .getUTCMinutes()
+        .toString()
+        .padStart(2, '0')}:${dateFormatted.getUTCSeconds().toString().padStart(2, '0')}`;
+      break;
+    }
+    case 'HH:mm': {
+      const dateFormatted = createDate(date);
+      if (!dateFormatted) {
+        return null;
+      }
+      hourString = `${dateFormatted.getUTCHours().toString().padStart(2, '0')}:${dateFormatted
+        .getUTCMinutes()
         .toString()
         .padStart(2, '0')}`;
       break;
