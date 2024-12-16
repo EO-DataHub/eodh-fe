1dbed3826089d3e2120ceb9076713a19be9c0866
mludwig <mlu@spyro-soft.com>
2024-12-16 14:45:32 +0100

    chore: code fix

--- libs/map/feature-action-creator-panel/src/lib/content/workflow/nodes/area/value-node.component.tsx
+++ libs/map/feature-action-creator-panel/src/lib/content/workflow/nodes/area/value-node.component.tsx
@@ -8,30 +8,39 @@ import { useTranslation } from 'react-i18next';
 
 import { Node } from '../node.component';
 import { NodeInput } from '../node-input.component';
+
 const SQUARE_KM_TO_SQUARE_M = 0.386102;
+
 const isCircle = (shape: Geometry): shape is Circle => shape.getType() === 'Circle';
+
 const getIconFromShape = (value: TAreaNode['value']): 'Polygon' | 'Circle' | 'Square' | undefined => {
   switch (value?.type) {
     case 'polygon': {
       return 'Polygon';
     }
+
     case 'circle': {
       return 'Circle';
     }
+
     case 'rectangle': {
       return 'Square';
     }
   }
 };
+
 export const convertUnits = (area: number, unit: 'km' | 'miles') => {
   let output;
+
   switch (unit) {
     case 'miles': {
       area = area * SQUARE_KM_TO_SQUARE_M;
       const value = Math.round((area / 1000000) * 100) / 100;
+
       output = `${value} miles<sup>2</sup>`;
       break;
     }
+
     case 'km': {
       if (area > 10000) {
         const value = Math.round((area / 1000000) * 100) / 100;
@@ -43,22 +52,32 @@ export const convertUnits = (area: number, unit: 'km' | 'miles') => {
       break;
     }
   }
+
   return output;
 };
+
 const getArea = (value: TAreaNode['value']): number => {
   const shape = createGeometry(value);
   if (!shape) {
     return 0;
   }
+
   const polygon = isCircle(shape) ? fromCircle(shape) : shape;
   return getAreaFromGeometry(polygon) || 0;
 };
+
 const formatArea = function (text: string, value: TAreaNode['value'], unit: 'km' | 'miles') {
   const area = getArea(value);
   const output = convertUnits(area, unit);
+
   return `${text.trim()} ${output}`;
 };
-type TValueNodeProps = { node: TAreaNode; onClearButtonClick: () => void };
+
+type TValueNodeProps = {
+  node: TAreaNode;
+  onClearButtonClick: () => void;
+};
+
 export const ValueNode = ({ node, onClearButtonClick }: TValueNodeProps) => {
   const { t } = useTranslation();
   const { canActivateNode, isLast } = useActionCreator();
@@ -70,15 +89,19 @@ export const ValueNode = ({ node, onClearButtonClick }: TValueNodeProps) => {
       }),
     [t, aoiLimit, measurmentUnit]
   );
+
   const getErrorMessage = useMemo(() => {
     if (!node.value) {
       return;
     }
+
     if (getArea(node.value) > aoiLimit) {
       return aoiLimitInfo;
     }
+
     return undefined;
   }, [aoiLimit, aoiLimitInfo, node.value]);
+
   return (
     <Node
       type={node.type}
@@ -89,12 +112,11 @@ export const ValueNode = ({ node, onClearButtonClick }: TValueNodeProps) => {
       hasNextNode={!isLast(node)}
       error={getErrorMessage}
     >
-      {' '}
       <NodeInput
         iconName={getIconFromShape(node.value)}
         value={t('MAP.ACTION_CREATOR_PANEL.WORKFLOW.NODE.AREA.DEFAULT_VALUE')}
         onClearButtonClick={onClearButtonClick}
-      />{' '}
+      />
     </Node>
   );
 };
