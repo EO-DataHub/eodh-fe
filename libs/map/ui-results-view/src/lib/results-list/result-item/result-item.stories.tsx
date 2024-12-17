339262f0d4368fa31648241e69fac7d7aa5ae5be
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2024-12-17 11:30:22 +0100

    fix: storybook build fix

--- libs/map/ui-results-view/src/lib/results-list/result-item/result-item.stories.tsx
+++ libs/map/ui-results-view/src/lib/results-list/result-item/result-item.stories.tsx
@@ -13,7 +13,7 @@ const imageUrlStoredInPublicAssetsInStorybookHost = '/assets/images/imageSample2
 export const SampleResultItem = {
   args: {
     imageUrl: imageUrlStoredInPublicAssetsInStorybookHost,
-    collectionName: 'Sentinel-1',
+    collection: 'Sentinel-1',
     dateTime: '2024-09-03T16:23:22.625Z',
     cloudCoverage: 39.5,
     gridCode: '33TTG',
