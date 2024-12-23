0ceed1c696b12159c80abb2d52289d2acadc97dc
Katarzyna Paszko <pkatarzynapaszko@gmail.com>
2024-12-23 13:22:14 +0100

    UKRIW-611 - Action Creator modals (#134)
    * feat: added UnloggedUserModal component and its logic
    
    * feat: unacive ac mode modal
    
    * chore: generated translations type
    
    * fix: different approach to html inside translation

--- libs/map/feature-action-creator-panel/src/lib/content/container.component.tsx
+++ libs/map/feature-action-creator-panel/src/lib/content/container.component.tsx
@@ -1,11 +1,20 @@
 import { PropsWithChildren } from 'react';
 
+import { UnactiveAcModeModal } from './modals/unactive-ac-mode-modal/unactive-ac-mode-modal.component';
+import { UnloggedUserModal } from './modals/unlogged-user-modal/unlogged-user-modal.component';
+
 export const Container = ({ children }: PropsWithChildren) => {
   return <div>{children}</div>;
 };
 
 export const Content = ({ children }: PropsWithChildren) => {
-  return <main className='bg-bright-main h-[450px] relative'>{children}</main>;
+  return (
+    <main className='bg-bright-main h-[450px] relative'>
+      {children}
+      <UnactiveAcModeModal />
+      <UnloggedUserModal />
+    </main>
+  );
 };
 
 export const Footer = ({ children }: PropsWithChildren) => {
