42fd71926711620ba62fab01fd3c00c2b97942d1
M Ludwig <3653161+nb0dy@users.noreply.github.com>
2025-11-01 14:02:13 +0100

    feat: support for proj:code and proj:epsg

--- libs/map/data-access-stac-catalog/src/lib/stac-model/workflow.schema.ts
+++ libs/map/data-access-stac-catalog/src/lib/stac-model/workflow.schema.ts
@@ -1,5 +1,7 @@
 import { TDateString } from '@ukri/shared/utils/date';
+import isNil from 'lodash/isNil';
 import isObject from 'lodash/isObject';
+import isString from 'lodash/isString';
 import z from 'zod';
 
 import { noDataSchema, thumbnailAssetSchema } from './asset.schema';
@@ -23,41 +25,65 @@ const propertySchema = z
     polarizations: data.polarizations as never,
   }));
 
-const waterQualityAssetSchema = z.object({
-  title: z.string(),
-  type: z.string(),
-  href: z.string(),
-  size: z.number(),
-  roles: z.array(z.string()),
-  nodata: noDataSchema,
-  'proj:epsg': z.number(),
-  'proj:shape': z.array(z.number()),
-  'proj:transform': z.array(z.number()),
-  'raster:bands': z.array(
-    z.object({
-      nodata: noDataSchema,
-      unit: z.string(),
-    })
-  ),
-  colormap: z.object({
-    max: z.number(),
-    min: z.number().nullable(),
-    mpl_equivalent_cmap: z.string(),
-    name: z.string(),
-    reversed: z.boolean(),
-    steps: z.number(),
-    units: z.string(),
-  }),
-  statistics: z.object({
-    maximum: z.number().nullable(),
-    mean: z.number().nullable(),
-    median: z.number().nullable(),
-    minimum: z.number().nullable(),
-    stddev: z.number().nullable(),
-    valid_percent: z.number().nullable(),
-  }),
-  'classification:classes': z.never().optional(),
-});
+const waterQualityAssetSchema = z.preprocess(
+  (input) => {
+    if (!isObject(input)) {
+      return input;
+    }
+
+    const data = input as Record<string, unknown>;
+    let projEpsg = data['proj:epsg'];
+
+    if (isNil(projEpsg) && !isNil(data['proj:code'])) {
+      projEpsg = data['proj:code'];
+
+      if (isString(projEpsg)) {
+        const match = projEpsg.split(':').pop();
+        projEpsg = match !== undefined ? parseInt(match, 10) : projEpsg;
+      }
+    }
+
+    return {
+      ...data,
+      'proj:epsg': projEpsg,
+    };
+  },
+  z.object({
+    title: z.string(),
+    type: z.string(),
+    href: z.string(),
+    size: z.number(),
+    roles: z.array(z.string()),
+    nodata: noDataSchema,
+    'proj:epsg': z.number(),
+    'proj:shape': z.array(z.number()),
+    'proj:transform': z.array(z.number()),
+    'raster:bands': z.array(
+      z.object({
+        nodata: noDataSchema,
+        unit: z.string(),
+      })
+    ),
+    colormap: z.object({
+      max: z.number(),
+      min: z.number().nullable(),
+      mpl_equivalent_cmap: z.string(),
+      name: z.string(),
+      reversed: z.boolean(),
+      steps: z.number(),
+      units: z.string(),
+    }),
+    statistics: z.object({
+      maximum: z.number().nullable(),
+      mean: z.number().nullable(),
+      median: z.number().nullable(),
+      minimum: z.number().nullable(),
+      stddev: z.number().nullable(),
+      valid_percent: z.number().nullable(),
+    }),
+    'classification:classes': z.never().optional(),
+  })
+);
 
 export const waterQualitySchema = featureGenericSchema.extend({
   properties: propertySchema
